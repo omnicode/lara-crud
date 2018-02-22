139a8a28dd6880ced69e642cdad5a5c552181c4d
davitbek <davitzeynalyan1991@gmail.com>
2018-02-22 17:51:29 +0400

    Update LaraCrudServiceProvider.php

--- src/ServiceProvider/LaraCrudServiceProvider.php
+++ src/ServiceProvider/LaraCrudServiceProvider.php
@@ -9,7 +9,6 @@ use LaraSupport\LaraServiceProvider;
 use LaraSupport\Str;
 use LaraTest\Console\Commands\MakeTestController;
 use LaraTest\Console\Commands\MakeTestModel;
-use LaraView\ServiceProvider\LaraViewServiceProvider;
 
 class LaraCrudServiceProvider extends LaraServiceProvider
 {
@@ -28,6 +27,6 @@ class LaraCrudServiceProvider extends LaraServiceProvider
      */
     public function register()
     {
-        $this->registerProviders(LaraViewServiceProvider::class);
+        
     }
 }
